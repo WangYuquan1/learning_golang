a9901095d0a5e7161d0a6a75f9b2c9a3958e4c0a
gerray <wyq0213@gmail.com>
2022-01-25 11:55:08 +0900

    DBアクセス追加

---
+++ dbsample.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func init() {
+	fmt.Printf("%s\n", "dbsample inited")
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func db_main() {
+	db, err := sql.Open("postgres", "user=postgres password=postgres dbname=mydb sslmode=disable")
+	checkErr(err)
+	// 插入数据
+	stmt, err := db.Prepare("INSERT INTO userinfo(username,department,created) VALUES($1,$2,$3) RETURNING uid")
+	checkErr(err)
+	res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
+	checkErr(err)
+	fmt.Println(res.RowsAffected())
+
+	var lastInsertId int
+	err = db.QueryRow("INSERT INTO userinfo(username,department,created) VALUES($1,$2,$3) RETURNING uid;", "astaxie", "研发部门", "2012-12-09").Scan(&lastInsertId)
+	checkErr(err)
+	fmt.Println("最后插入id =", lastInsertId)
+
+	// 更新数据
+	stmt, err = db.Prepare("update userinfo set username=$1 where uid=$2")
+	checkErr(err)
+	res, err = stmt.Exec("astaxieupdate", 1)
+	checkErr(err)
+	affect, err := res.RowsAffected()
+	checkErr(err)
+	fmt.Println(affect)
+
+	// 查询数据
+	rows, err := db.Query("SELECT * FROM userinfo")
+	checkErr(err)
+	for rows.Next() {
+		var uid int
+		var username string
+		var department string
+		var created string
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println(uid)
+		fmt.Println(username)
+		fmt.Println(department)
+		fmt.Println(created)
+	}
+
+	// 删除数据
+	stmt, err = db.Prepare("delete from userinfo where uid=$1")
+	checkErr(err)
+	res, err = stmt.Exec(1)
+	checkErr(err)
+	affect, err = res.RowsAffected()
+	checkErr(err)
+	fmt.Println(affect)
+
+	db.Close()
+}
--- main.go
+++ main.go
@@ -2,5 +2,6 @@ package main
 
 func main() {
 	//learning()    // helloword.go
+	db_main()
 	main_sample() // web.go
 }
